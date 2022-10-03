aee25e9689da0e9538c6e56e7664b189accec5c1
mihaifirst <varzaru.alexmihai@gmail.com>
2022-10-04 01:18:50 +0300

    created user schema.js

---
+++ api/app/modules/user/user.schema.js
@@ -0,0 +1,13 @@
+const { mongoose } = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  isAdmin: Boolean,
+});
+
+const usersCollection = mongoose.model("User", userSchema);
+module.exports = usersCollection;
